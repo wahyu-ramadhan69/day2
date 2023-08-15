c6a86e507cc537131a45b46629330e9875cc19d0
wahyu-ramadhan69 <wahyu.ramadhani6969@gmail.com>
2023-08-15 09:20:41 +0700

    coba1

---
+++ src/main/java/com/bcafinance/Main.java
@@ -0,0 +1,8 @@
+package com.bcafinance;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("tai");
+    }
+}
